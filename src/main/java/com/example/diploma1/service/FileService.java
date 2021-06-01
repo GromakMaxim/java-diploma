cdff114a18d6a670a1d68a3cb7c3254997fc127d
Gromak Maxim <maximgromak@gmail.com>
2021-06-01 22:01:19 +1000

    p7

--- src/main/java/com/example/diploma1/service/FileService.java
+++ src/main/java/com/example/diploma1/service/FileService.java
@@ -3,7 +3,7 @@ package com.example.diploma1.service;
 import com.example.diploma1.model.IncomingFile;
 import com.example.diploma1.repository.FileRepository;
 import com.example.diploma1.security.JwtTokenUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,25 +12,19 @@ import javax.transaction.Transactional;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class FileService{
 
     private FileRepository fileRepository;
     private UserService userService;
     private JwtTokenUtil jwtTokenUtil;
 
-    public FileService(FileRepository fileRepository, UserService userService, JwtTokenUtil jwtTokenUtil) {
-        this.fileRepository = fileRepository;
-        this.userService = userService;
-        this.jwtTokenUtil = jwtTokenUtil;
-    }
-
     @Transactional(rollbackOn = IOException.class)//в случае падения IOException, все наши сохранения в БД откатятся?
     public void upload(MultipartFile resource, HttpServletRequest request) throws IOException {
-        String tokenRaw = request.getHeader("auth-token");
+        var tokenRaw = request.getHeader("auth-token");
         var usernameFromToken = jwtTokenUtil.getUserNameFromTokenRaw(tokenRaw);
         var user = userService.getUserByLoginReturnUser(usernameFromToken);
 
@@ -47,8 +41,7 @@ public class FileService{
     }
 
     public IncomingFile download(String filename) {
-        Optional<IncomingFile> file = fileRepository.findByFilename(filename);
-        return file.isPresent() ? file.get() : null;
+        return fileRepository.findByFilename(filename).orElse(null);
     }
 
     public void delete(String filename, String username) {
