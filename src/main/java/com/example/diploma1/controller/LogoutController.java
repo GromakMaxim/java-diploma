c7aab47fb1b9b74eddad55f9c27be18454f629ff
Gromak Maxim <maximgromak@gmail.com>
2021-06-03 00:19:32 +1000

    p11

--- src/main/java/com/example/diploma1/controller/LogoutController.java
+++ src/main/java/com/example/diploma1/controller/LogoutController.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +19,7 @@ public class LogoutController {
 
     private UserService userService;
 
-    @GetMapping(value = "/login")
+    @PostMapping("/logout")
     public ResponseEntity<?> logout(@RequestHeader("User-Agent") String useragent, HttpServletRequest request) {
         var ip = request.getRemoteAddr();
         var hostname = request.getRemoteHost();
